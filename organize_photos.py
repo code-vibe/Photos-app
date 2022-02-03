76639e0c60bf4f9460d7e48b6070917ff0401509
code-vibe <mrsamoladapo@gmail.com>
2022-02-03 20:46:28 +0100

    Updated organize_photo file.

--- organize_photos.py
+++ organize_photos.py
@@ -1,10 +1,17 @@
 import os
+
 def extract_place(filename):
     return filename.split("_")[1]
+
+def make_place_directories(places):
+	for place in places:
+		os.mkdir(place)
+	
 
 os.chdir("Photos")
 originals = os.listdir()
 places = []
+
 
 for filename in originals:
 	place = extract_place(filename)
